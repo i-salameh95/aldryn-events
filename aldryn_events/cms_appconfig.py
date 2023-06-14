97577c22ab2ca544bdbcf2613f4668e76cabc20b
i.salameh95 <i.salameh@najah.edu>
2023-06-14 12:16:37 +0300

    remove python2

--- aldryn_events/cms_appconfig.py
+++ aldryn_events/cms_appconfig.py
@@ -4,7 +4,7 @@
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
+
 
 from aldryn_apphooks_config.models import AppHookConfig
 from cms.models.fields import PlaceholderField
